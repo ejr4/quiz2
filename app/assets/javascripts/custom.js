d6c1d274348f3bd075810aae637194ea6e9e8b80
Eric <ejr4@pm.me>
2018-05-28 16:32:15 -0700

    plus and minus toggles all at once

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,4 +1,23 @@
+
+
 $(() => {
 
+    $(".idea-description").hide();
+
+    $(".toggler").on("click", event => {
+        const { currentTarget } = event;
+        // You can still use currentTarget inside a listener passed to
+        // to on, but don't forget wrap in a Jquery Collection.
+        $(".idea-description").hide();
+        $(".plus-sign").show();
+      });
+    
+      $(".plus-sign").on("click", event => {
+        const { currentTarget } = event;
+        // You can still use currentTarget inside a listener passed to
+        // to on, but don't forget wrap in a Jquery Collection.
+        $(".idea-description").show();
+        $(".plus-sign").hide();
+      });
 
 });
